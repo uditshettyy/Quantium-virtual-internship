ee63a9ad6ca016aa56b62a3c6524b146b1648a2d
udit shetty <uditshetty091@gmail.com>
2026-03-05 18:12:55 +0530

    3rd commit

--- process_data.py
+++ process_data.py
@@ -6,8 +6,9 @@ df3=pd.read_csv('data/daily_sales_data_2.csv')
 df=pd.concat([df1,df2,df3],ignore_index=True)
 
 df=df[df["product"]=="pink morsel"]
+df["price"]=df["price"].str.replace("$","",regex=False).astype(float)
 
-df["sales"]=df["quantity"]*df["price"]
+df["sales"]=df["quantity"].astype(int)*df["price"]
 
 df=df[["sales","date","region"]]
 
